bd54028a691995ccb54a1b5ccd708e4d1dc438ac
suibaka <suibaka@hotmail.co.jp>
2019-10-28 17:14:39 +0900

    print detailed results

--- src/cui.ml
+++ src/cui.ml
@@ -4,6 +4,26 @@ open Intuitionistic_typed
 open Decorated_typed
 open Eval
 
+let string_of_qs qs =
+  let rec inner n =
+    if n = 0 then []
+    else (Printf.sprintf "q%d" (n - 1)) :: inner (n - 1)
+  in
+  "|" ^ (String.concat ~sep:"," (inner qs)) ^ ">"
+
+let binary_of_int i n =
+  let rec inner j =
+    (if i land (1 lsl j) <> 0 then "1" else "0")
+    ^
+    if j = 0 then "" else inner (j - 1)
+  in inner (n - 1)
+
+let print_results (states, qs, v) =
+  Printf.printf "Results:\n Q =\n";
+  List.iteri states ~f:(fun i a -> Printf.printf "  |%s> with %f\n" (binary_of_int i qs) a);
+  Printf.printf " L = %s\n" (string_of_qs qs);
+  Printf.printf " value = %s\n\n" (string_of_value v)
+
 let read_eval_print env tyenv =
   print_string "# "; Out_channel.flush stdout;
   let t = Parser.toplevel Lexer.main (Lexing.from_channel In_channel.stdin) in
@@ -14,11 +34,11 @@ let read_eval_print env tyenv =
 
   (* decorated type *)
   let typed_t = ty_dterm typed_t in
-  Printf.printf "\nDecorated typed term : \n %s\n\n" (string_of_dterm typed_t);
+  Printf.printf "Decorated typed term : \n %s\n\n" (string_of_dterm typed_t);
 
   (* evaluation *)
-  let _, _, v = eval_term env (Array.create ~len:0 0., 0, t) in
-  Printf.printf "\nvalue = %s\n\n" (string_of_value v)
+  let states, qs, v = eval_term env (Array.create ~len:0 0., 0, t) in
+  print_results (Array.to_list states, qs, v)
 
 
 (* for test *)
